db603884143e64181b02b36e71fc7417238db860
Mamun123123123 <almalm90459@gmail.com>
2025-11-01 09:53:09 +0600

    successfully Data send

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Middleware
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 
-// MySQL Connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",      // যদি password থাকে তবে লিখবে
+  password: "",     
   database: "registrationDB",
 });
 
@@ -23,7 +22,6 @@ db.connect((err) => {
   console.log("MySQL Connected...");
 });
 
-// POST Route
 app.post("/submit", (req, res) => {
   const { name, email, password } = req.body;
   const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
@@ -33,12 +31,10 @@ app.post("/submit", (req, res) => {
   });
 });
 
-// Serve registration.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "registration.html"));
 });
 
-// Start Server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
